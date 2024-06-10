00b8434511ec61adf6e5c9db44490d0e701f5659
Nastya <elgina.anasta@yandex.ru>
2024-06-10 11:32:27 +0300

    Created Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -9,7 +9,9 @@ export const AppRouter = () => {
 				{Object.values(routeConfig).map(({element, path}) => (
 					<Route
 						key={path}
-						element={element}
+						element={(
+							<div className="page-wrapper">{element}</div>
+						)}
 						path={path}
 					/>
 				))}
